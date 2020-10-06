f780d0dc0803604c55e67e06f013c6ded2a23b3a
esfn <christianweb@protonmail.com>
2020-10-06 14:40:24 -0700

    init project

---
+++ src/js/views/wordcabView.js
@@ -0,0 +1,10 @@
+(function(window) {
+    'use strict';
+
+    const wordcabView = {}
+
+    console.log('wordcabView init');
+    
+    window.app = window.app || {};
+    window.app.wordcabView = wordcabView;
+})(window);
